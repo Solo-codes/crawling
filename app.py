c290feb82b67ce90e48d8ccaac0ab29d92f36171
Solo-codes <Vkjr29@gmail.com>
2023-07-25 15:42:19 +0530

    new commit2

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ app = Flask(__name__)
 cors = CORS(app)
 
 # geckodriver_path = r'C:/flask_app/scrub/geckodriver.exe'  # Replace with the actual path to geckodriver
-geckodriver_path = r'/snap/bin/geckodriver'
-firefox_binary_path = r'/usr/bin/firefox'  # Replace with the actual path to Firefox binary
+geckodriver_path = "/snap/bin/geckodriver"
+firefox_binary_path = "/usr/bin/firefox"  # Replace with the actual path to Firefox binary
 @app.route('/', methods=['GET', 'POST'])
 def scrape_website():
     if request.method == 'POST':
